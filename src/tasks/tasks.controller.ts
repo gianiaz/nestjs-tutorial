b4d15a0797125482a3215943538e35750fde8a4d
Giovanni Lenoci <giovanni.lenoci@facile.it>
2022-09-25 17:13:00 +0200

    Type safety and list of crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
-  helloWorld() {
-    this.taskService
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
   }
 }
