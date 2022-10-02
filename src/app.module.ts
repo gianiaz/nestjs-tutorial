a9e9bd261763f4c322d44be3f48b3850fa4a66c8
Giovanni Lenoci <gianiaz@gmail.com>
2022-10-02 22:33:51 +0200

    Adds database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nestjs',
+      autoLoadEntities: true,
+    }),
+  ],
 })
 export class AppModule {}
